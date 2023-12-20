48e8d7f5933ddedcc6dd2ba658c44908c4c5e094
van huong <tovanhuong007@gmail.com>
2023-12-20 22:41:24 +0700

    don't use inviroment variables

--- src/firebase_setup/firebase.js
+++ src/firebase_setup/firebase.js
@@ -3,14 +3,26 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_API_KEY,
+//   authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+//   projectId: import.meta.env.VITE_PROJECT_ID,
+//   storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+//   messagingSenderId: import.meta.env.VITE_MESSAGE_SENDER_ID,
+//   appId: import.meta.env.VITE_APP_ID,
+//   measurementId: import.meta.env.VITE_MEASUREMENT_ID,
+// };
+
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGE_SENDER_ID,
-  appId: import.meta.env.VITE_APP_ID,
-  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
+  apiKey: "AIzaSyB4WmvbpLvN4VB9VXOKXDuzvpAI9hU2pFw",
+  authDomain: "cumulative-points-calcul-73190.firebaseapp.com",
+  databaseURL:
+    "https://cumulative-points-calcul-73190-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "cumulative-points-calcul-73190",
+  storageBucket: "cumulative-points-calcul-73190.appspot.com",
+  messagingSenderId: "995137744269",
+  appId: "1:995137744269:web:0ea304473383e4be3b61c4",
+  measurementId: "G-PSSDZ70MMY",
 };
 
 // Initialize Firebase
